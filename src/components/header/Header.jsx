f2f5830b45cb7096d53588173b6f056e0f4c3684
mprice97 <mprice97@charlotte.edu>
2026-04-01 14:35:01 -0400

    update checkout page and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,24 @@
 import './header.css'
+import { FaUser, FaBars, FaHome } from 'react-icons/fa'
+import logo from '../../assets/logo.png' 
 
-const Header = ({ headerHeight= '18vh'}) => {
-	return(
-		<div className="header"
-			style={{
-				color: '#6A1A2F',
-				height: headerHeight
-			}}
-		>
-			{/* here is where header contents goes*/}
-		</div>
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="header_left">
+				<img src={logo} alt="logo" className="logo" />
+			</div>
+
+			<div className="header_center">
+				<h1>Trip Booker</h1>
+			</div>
+
+			<div className="header_right">
+				<div className="icon"><FaUser /></div>
+				
+			</div>
+		</header>
 	)
 }
 
-export default Header;
+export default Header
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,12 +1,142 @@
 import './checkout.css'
+import tripMain from '../../assets/trip-main.jpg'
+import tripBottom from '../../assets/trip-bottom.jpg'
+import { FaHome, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 const Checkout = () => {
-	return(
+	return (
 		<div className="checkout_page">
-			This is the checkout page
-			{/* add html for checkout page here, stylesheet is checkout.css */}
+			<div className="checkout_content">
+				<div className="checkout_left">
+					<section className="form_section">
+						<h2>Whos Traveling?</h2>
+
+						<div className="form_row">
+							<label>First Name:</label>
+							<input type="text" />
+						</div>
+
+						<div className="form_row">
+							<label>Last Name:</label>
+							<input type="text" />
+						</div>
+
+						<div className="form_row">
+							<label>Email:</label>
+							<input type="email" />
+						</div>
+
+						<div className="form_row">
+							<label>Phone:</label>
+							<input type="text" />
+						</div>
+					</section>
+
+					<section className="form_section">
+						<h2>Payment Method</h2>
+
+						<div className="form_row">
+							<label>Card Name:</label>
+							<input type="text" />
+						</div>
+
+						<div className="form_row">
+							<label>Card #:</label>
+							<input type="text" />
+						</div>
+
+						<div className="payment_small_row">
+							<div className="small_field">
+								<label>Exp:</label>
+								<input type="text" />
+							</div>
+
+							<div className="small_field">
+								<label>CVV:</label>
+								<input type="text" />
+							</div>
+						</div>
+					</section>
+
+					<section className="form_section">
+						<h2>Billing Address</h2>
+
+						<div className="form_row">
+							<label>Address:</label>
+							<input type="text" />
+						</div>
+
+						<div className="form_row">
+							<label>City:</label>
+							<input type="text" />
+						</div>
+
+						<div className="billing_split">
+							<div className="small_field">
+								<label>State:</label>
+								<input type="text" />
+							</div>
+
+							<div className="small_field">
+								<label>Zip:</label>
+								<input type="text" />
+							</div>
+						</div>
+					</section>
+				</div>
+
+				<div className="checkout_middle">
+					<div className="image_stack">
+						<img src={tripMain} alt="Tropical resort view" />
+						<img src={tripBottom} alt="Beachfront patio view" />
+					</div>
+				</div>
+
+				<div className="checkout_right">
+					<section className="summary_section">
+						<h2>Trip Summary</h2>
+
+						<div className="summary_line">
+							<span>Location:</span>
+							<span></span>
+						</div>
+
+						<div className="summary_line">
+							<span>Days:</span>
+							<span></span>
+						</div>
+
+						<div className="summary_line">
+							<span>People:</span>
+							<span></span>
+						</div>
+					</section>
+
+					<section className="price_box">
+						<h3>Price Summary</h3>
+						<p># Rooms x # Nights</p>
+
+						<div className="price_line">Tickets</div>
+						<div className="price_line">Tax and Fees</div>
+						<div className="price_total">Total:</div>
+					</section>
+
+					<button className="confirm_btn">Confirm Checkout</button>
+				</div>
+			</div>
+
+			<div className="checkout_footer_bar">
+				<div className="footer_left">
+					<FaHome size={28} />
+				</div>
+
+				<div className="footer_right">
+					<FaArrowLeft size={24} />
+					<FaArrowRight size={24} />
+				</div>
+			</div>
 		</div>
 	)
 }
 
-export default Checkout;
+export default Checkout
