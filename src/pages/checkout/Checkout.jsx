63a34439a3d0f51e7c85e2b2f273812942cdd9d1
TrishaGite-26 <trishgitee@gmail.com>
2026-04-02 13:48:38 -0400

    Homepage
    Worked on homepage elements.

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -4,6 +4,7 @@ const Checkout = () => {
 	return(
 		<div className="checkout_page">
 			This is the checkout page
+			<h1 style={{textAlign: 'center'}}>Welcome to the checkout page!</h1>
 			{/* add html for checkout page here, stylesheet is checkout.css */}
 		</div>
 	)
