8b86199a56a596df6c6d920d9be1312228c3c953
Nathaniel Joseph <njoseph8@charlotte.edu>
2026-04-02 12:27:10 -0400

    Update Searchresult.jsx

--- src/pages/searchresult/Searchresult.jsx
+++ src/pages/searchresult/Searchresult.jsx
@@ -4,11 +4,20 @@ const Searchresult = () => {
 	return(
 		<div className="search_result">
 			<div className="tabs">
-				<div className = "tab">Hotel</div>
-				<div className = "tab active">Homes</div>
-
-
+				<div className="tab">Hotel</div>
+				<div className="tab active">Homes</div>
+				<div className="tab">Stays</div>
 			</div>
+
+			<input
+				type="text"
+				ClassName="search_box"
+				placeholder="Search a property"
+			/>
+
+			<p className="filters">Filters:</p>
+
+			
 		</div>
 	)
 }
