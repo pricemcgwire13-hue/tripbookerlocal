a9804ed5cf946fce4f2e23d791be97f1fbc89ece
mprice97 <mprice97@charlotte.edu>
2026-04-03 14:52:38 -0400

    added nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,31 @@
 import './header.css'
-import { FaUser, FaBars, FaHome } from 'react-icons/fa'
-import logo from '../../assets/logo.png' 
+import { FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
 
 const Header = () => {
 	return (
-		<header className="header">
-			<div className="header_left">
-				<img src={logo} alt="logo" className="logo" />
-			</div>
+		<>
+			<header className="header">
+				<div className="header_left">
+					<img src={logo} alt="logo" className="logo" />
+				</div>
 
-			<div className="header_center">
-				<h1>Trip Booker</h1>
-			</div>
+				<div className="header_center">
+					<h1>Trip Booker</h1>
+				</div>
 
-			<div className="header_right">
-				<div className="icon"><FaUser /></div>
-				
-			</div>
-		</header>
+				<div className="header_right">
+					<div className="icon"><FaUser /></div>
+				</div>
+			</header>
+
+			<nav className="sub_nav">
+				<Link to="/">Home</Link>
+				<Link to="/searchresult">Search Results</Link>
+				<Link to="/checkout">Checkout</Link>
+			</nav>
+		</>
 	)
 }
 
