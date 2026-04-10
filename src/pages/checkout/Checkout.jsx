033c48317687806d20627c81e76e7c4aa18239cb
mprice97 <mprice97@charlotte.edu>
2026-04-09 23:45:05 -0400

    updated checkout page and header

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -3,32 +3,86 @@ import tripMain from '../../assets/trip-main.jpg'
 import tripBottom from '../../assets/trip-bottom.jpg'
 import { FaHome, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
+const states = [
+	'Alabama',
+	'Alaska',
+	'Arizona',
+	'Arkansas',
+	'California',
+	'Colorado',
+	'Connecticut',
+	'Delaware',
+	'Florida',
+	'Georgia',
+	'Hawaii',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Iowa',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Maine',
+	'Maryland',
+	'Massachusetts',
+	'Michigan',
+	'Minnesota',
+	'Mississippi',
+	'Missouri',
+	'Montana',
+	'Nebraska',
+	'Nevada',
+	'New Hampshire',
+	'New Jersey',
+	'New Mexico',
+	'New York',
+	'North Carolina',
+	'North Dakota',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Pennsylvania',
+	'Rhode Island',
+	'South Carolina',
+	'South Dakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Vermont',
+	'Virginia',
+	'Washington',
+	'West Virginia',
+	'Wisconsin',
+	'Wyoming'
+]
+
+
 const Checkout = () => {
 	return (
 		<div className="checkout_page">
 			<div className="checkout_content">
 				<div className="checkout_left">
 					<section className="form_section">
-						<h2>Whos Traveling?</h2>
+						<h2>Who’s Traveling?</h2>
 
 						<div className="form_row">
-							<label>First Name:</label>
-							<input type="text" />
+							<label htmlFor="firstName">First Name</label>
+							<input id="firstName" type="text" placeholder="Enter first name" />
 						</div>
 
 						<div className="form_row">
-							<label>Last Name:</label>
-							<input type="text" />
+							<label htmlFor="lastName">Last Name</label>
+							<input id="lastName" type="text" placeholder="Enter last name" />
 						</div>
 
 						<div className="form_row">
-							<label>Email:</label>
-							<input type="email" />
+							<label htmlFor="email">Email</label>
+							<input id="email" type="email" placeholder="Enter email" />
 						</div>
 
 						<div className="form_row">
-							<label>Phone:</label>
-							<input type="text" />
+							<label htmlFor="phone">Phone</label>
+							<input id="phone" type="text" placeholder="Enter phone number" />
 						</div>
 					</section>
 
@@ -36,24 +90,24 @@ const Checkout = () => {
 						<h2>Payment Method</h2>
 
 						<div className="form_row">
-							<label>Card Name:</label>
-							<input type="text" />
+							<label htmlFor="cardName">Card Name</label>
+							<input id="cardName" type="text" placeholder="Name on card" />
 						</div>
 
 						<div className="form_row">
-							<label>Card #:</label>
-							<input type="text" />
+							<label htmlFor="cardNumber">Card Number</label>
+							<input id="cardNumber" type="text" placeholder="1234 5678 9012 3456" />
 						</div>
 
 						<div className="payment_small_row">
 							<div className="small_field">
-								<label>Exp:</label>
-								<input type="text" />
+								<label htmlFor="exp">Exp Date</label>
+								<input id="exp" type="text" placeholder="MM/YY" />
 							</div>
 
 							<div className="small_field">
-								<label>CVV:</label>
-								<input type="text" />
+								<label htmlFor="cvv">CVV</label>
+								<input id="cvv" type="text" placeholder="123" />
 							</div>
 						</div>
 					</section>
@@ -62,24 +116,33 @@ const Checkout = () => {
 						<h2>Billing Address</h2>
 
 						<div className="form_row">
-							<label>Address:</label>
-							<input type="text" />
+							<label htmlFor="address">Address</label>
+							<input id="address" type="text" placeholder="Street address" />
 						</div>
 
 						<div className="form_row">
-							<label>City:</label>
-							<input type="text" />
+							<label htmlFor="city">City</label>
+							<input id="city" type="text" placeholder="Enter city" />
 						</div>
 
 						<div className="billing_split">
 							<div className="small_field">
-								<label>State:</label>
-								<input type="text" />
+								<label htmlFor="state">State</label>
+								<select id="state" defaultValue="">
+									<option value="" disabled>
+										Select state
+									</option>
+									{states.map((state) => (
+										<option key={state} value={state}>
+											{state}
+										</option>
+									))}
+								</select>
 							</div>
 
 							<div className="small_field">
-								<label>Zip:</label>
-								<input type="text" />
+								<label htmlFor="zip">ZIP</label>
+								<input id="zip" type="text" placeholder="28223" />
 							</div>
 						</div>
 					</section>
@@ -97,28 +160,44 @@ const Checkout = () => {
 						<h2>Trip Summary</h2>
 
 						<div className="summary_line">
-							<span>Location:</span>
-							<span></span>
+							<span>Location</span>
+							<span>St. Lucia</span>
 						</div>
 
 						<div className="summary_line">
-							<span>Days:</span>
-							<span></span>
+							<span>Days</span>
+							<span>5</span>
 						</div>
 
 						<div className="summary_line">
-							<span>People:</span>
-							<span></span>
+							<span>Travelers</span>
+							<span>2</span>
 						</div>
 					</section>
 
 					<section className="price_box">
 						<h3>Price Summary</h3>
-						<p># Rooms x # Nights</p>
+						<p>1 Room × 5 Nights</p>
 
-						<div className="price_line">Tickets</div>
-						<div className="price_line">Tax and Fees</div>
-						<div className="price_total">Total:</div>
+						<div className="price_line">
+							<span>Room Total</span>
+							<span>$1,450</span>
+						</div>
+
+						<div className="price_line">
+							<span>Tickets</span>
+							<span>$820</span>
+						</div>
+
+						<div className="price_line">
+							<span>Tax & Fees</span>
+							<span>$210</span>
+						</div>
+
+						<div className="price_total">
+							<span>Total</span>
+							<span>$2,480</span>
+						</div>
 					</section>
 
 					<button className="confirm_btn">Confirm Checkout</button>
@@ -127,12 +206,12 @@ const Checkout = () => {
 
 			<div className="checkout_footer_bar">
 				<div className="footer_left">
-					<FaHome size={28} />
+					<FaHome size={24} />
 				</div>
 
 				<div className="footer_right">
-					<FaArrowLeft size={24} />
-					<FaArrowRight size={24} />
+					<FaArrowLeft size={20} />
+					<FaArrowRight size={20} />
 				</div>
 			</div>
 		</div>
